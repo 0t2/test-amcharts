1c4b33ae287036daa3ade6ffb6f6ed4cce425433
Jesse Chen <0t2jesse@gmail.com>
2017-05-23 14:29:35 +0800

    Simple example.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AmChartsModule } from "@amcharts/amcharts3-angular";
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AmChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
